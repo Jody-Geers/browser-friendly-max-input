33714ed1f207e1cc1bee8e74bef15167edc9b4a4
Jody Jacobus Geers <y2k_geers@hotmail.com>
2016-08-15 09:58:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 /*
-  "repo": "Jody-Geers/max-input",
-  "description": "browser fwiendly max input validation.",
+"repo": "Jody-Geers/max-input",
+"description": "browser fwiendly max input validation.",
 */
 
 
 /**
 * Browser friendly max input validation.
-* @param {string} args.el - The context element.
-* @param {string} args.targetId - The output ui element id for count.
+* @param {dom event} event - onkeyup event listener
+* @access private
 */
-function _limitUserInput ( event ) {
-  var _maxLength = event.currentTarget.maxLength;
-  // include new lines as count of 2
-  var _newLines = event.currentTarget.value.match(/(\r\n|\n|\r)/g);
-  var _addition = 0;
+function limitUserInput( event ) {
+	
+	var maxLength = event.currentTarget.maxLength;
+	// include new lines as count of 2
+	var newLines = event.currentTarget.value.match( /(\r\n|\n|\r)/g );
+	var addition = 0;
 
-  if ( _newLines != null ) _addition = _newLines.length;
+	if ( newLines != null ) addition = newLines.length;
 
-  // update UI
-  if ( ( (event.currentTarget.value.length + _addition) - _maxLength ) > 0 ) {
-    event.currentTarget.value = event.currentTarget.value.slice( 0, -( (event.currentTarget.value.length + _addition) - _maxLength ) );
-  }
-  
-  if ( event.currentTarget.targetId )
-    document.getElementById(event.currentTarget.targetId).innerHTML = _maxLength - (event.currentTarget.value.length + _addition);
+	// update UI
+	if ( ( ( event.currentTarget.value.length + addition ) - maxLength ) > 0 ) {
+		event.currentTarget.value = event.currentTarget.value.slice( 0, -( ( event.currentTarget.value.length + addition ) - maxLength ) );
+	}
+
+	if ( event.currentTarget.targetId )
+		document.getElementById( event.currentTarget.targetId ).innerHTML = maxLength - ( event.currentTarget.value.length + addition );
+
 };
 
 
 /*
 * visible API - Functionality
+* @param {string} args.elId - The context element id.
+* @param {string} args.targetId - The output ui element id for count.
+* @access public
 */
 function attachMaxInputValidation ( args ) {
-  var el = document.getElementById( args.elId );
-    el.targetId = args.targetId;
-    el.onkeyup = _limitUserInput;
+	
+	var el = document.getElementById( args.elId );
+		el.targetId = args.targetId;
+		el.onkeyup = limitUserInput;
+	
 };
 
 module.exports.attachMaxInputValidation = attachMaxInputValidation;
